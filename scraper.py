dc5f108562d1535add48b3e4985811c3055ce14c
William Petroff <willpetroff@gmail.com>
2017-02-27 01:38:44 -0500

    scraper update

--- scraper.py
+++ scraper.py
@@ -10,38 +10,40 @@ HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/201
           'Referrer': 'http://www.palmtran.org/igo'}
 
 target = 'http://www.palmtran.org/iGo/rest/Vehicles/GetAllVehicles'
+target = 'http://www.palmtran.org/iGo/rest/Vehicles/GetAllVehiclesForRoute?routeID=43'
 
 
 
 request = get(target, headers=HEADER)
 while request.status_code == 200:
-    with open('palmtran.csv', 'a', newline='') as csv_file:
-        data = [ [
-                     item['TripId'],
-                     item['RouteId'],
-                     item['RunId'],
-                     item['VehicleId'],
-                     item['Latitude'],
-                     item['Longitude'],
-                     item['Speed'],
-                     item['Deviation'],
-                     item['LastStop'],
-                     item['Direction'],
-                     item['DirectionLong'],
-                     item['OnBoard'],
-                     item['Destination'],
-                     item['Name'],
-                     int(''.join([char for char in item['LastUpdated'] if char in digits or char is '-'][:-5])) / 1000,
-                     item['Heading'],
-                     item['OpStatus'],
-                     item['DisplayStatus'],
-                     item['GPSStatus'],
-                     item['CommStatus'],
-                     item['BlockFareboxId'],
-                     item['DriverName'],
-                 ] for item in request.json()]
-        writer = csv.writer(csv_file)
-        writer.writerows(data)
+    if request.json():
+        with open('palmtran.csv', 'a', newline='') as csv_file:
+            data = [ [
+                         item['TripId'],
+                         item['RouteId'],
+                         item['RunId'],
+                         item['VehicleId'],
+                         item['Latitude'],
+                         item['Longitude'],
+                         item['Speed'],
+                         item['Deviation'],
+                         item['LastStop'],
+                         item['Direction'],
+                         item['DirectionLong'],
+                         item['OnBoard'],
+                         item['Destination'],
+                         item['Name'],
+                         int(''.join([char for char in item['LastUpdated'] if char in digits or char is '-'][:-5])) / 1000,
+                         item['Heading'],
+                         item['OpStatus'],
+                         item['DisplayStatus'],
+                         item['GPSStatus'],
+                         item['CommStatus'],
+                         item['BlockFareboxId'],
+                         item['DriverName'],
+                     ] for item in request.json()]
+            writer = csv.writer(csv_file)
+            writer.writerows(data)
     sleep(45)
     request = get(target, headers=HEADER)
 print(str(datetime.now()), request.status_code)
